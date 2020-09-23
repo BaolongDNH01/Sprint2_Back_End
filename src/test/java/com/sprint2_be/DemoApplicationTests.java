fbce73c239b648f38c2b6afb368c009039454825
BaoLongDNH01 <duongtranbaolongdn@gmail.com>
2020-09-23 10:38:09 +0700

    first commit

---
+++ src/test/java/com/sprint2_be/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint2_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

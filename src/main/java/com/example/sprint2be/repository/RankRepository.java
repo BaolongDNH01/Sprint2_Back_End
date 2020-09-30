d6f4bdc93b6099a1b8af00446c34e67a1b56d797
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-09-30 22:07:22 +0700

    quan

--- src/main/java/com/example/sprint2be/repository/RankRepository.java
+++ src/main/java/com/example/sprint2be/repository/RankRepository.java
@@ -1,4 +1,7 @@
 package com.example.sprint2be.repository;
 
-public class RankRepository {
+import com.example.sprint2be.model.Rank;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RankRepository extends JpaRepository<Rank, Integer> {
 }

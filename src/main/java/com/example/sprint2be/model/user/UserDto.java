f8b74ff1a2c89c663add3f05d2cbf2909a5ab7c7
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-09-30 22:06:15 +0700

    quan

--- src/main/java/com/example/sprint2be/model/user/UserDto.java
+++ src/main/java/com/example/sprint2be/model/user/UserDto.java
@@ -1,4 +1,132 @@
 package com.example.sprint2be.model.user;
 
+
 public class UserDto {
+    private Integer id;
+    private String name;
+    private String account;
+    private String password;
+    private String email;
+    private String phone;
+    private String birthday;
+    private String address;
+    private String idCard;
+    private String point;
+    private String signInRecent;
+    private String timeLock;
+    private String avatar;
+    private Integer rank;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getPoint() {
+        return point;
+    }
+
+    public void setPoint(String point) {
+        this.point = point;
+    }
+
+    public String getSignInRecent() {
+        return signInRecent;
+    }
+
+    public void setSignInRecent(String signInRecent) {
+        this.signInRecent = signInRecent;
+    }
+
+    public String getTimeLock() {
+        return timeLock;
+    }
+
+    public void setTimeLock(String timeLock) {
+        this.timeLock = timeLock;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+
 }

fa1f282f1f362ffd16d9e6690165bbc03c0ff952
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-09-30 22:08:24 +0700

    quan

--- src/main/java/com/example/sprint2be/repository/UserRepository.java
+++ src/main/java/com/example/sprint2be/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.sprint2be.repository;
 
-public class UserRepository {
+import com.example.sprint2be.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }

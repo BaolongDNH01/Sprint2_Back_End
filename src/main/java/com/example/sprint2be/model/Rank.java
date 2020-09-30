dc2b5910153a8de86c119294a47b993bfcfc87f5
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-09-30 22:04:27 +0700

    quan

--- src/main/java/com/example/sprint2be/model/Rank.java
+++ src/main/java/com/example/sprint2be/model/Rank.java
@@ -1,4 +1,44 @@
 package com.example.sprint2be.model;
 
+
+import com.example.sprint2be.model.user.User;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "_rank")
 public class Rank {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Id tu tang, khong can them id
+    private Integer rankId;
+    private String name;
+    //@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+   // @JsonManagedReference
+    //private Set<User> users;
+
+    public Integer getRankId() {
+        return rankId;
+    }
+
+    public void setRankId(Integer rankId) {
+        this.rankId = rankId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+   // public Set<User> getUsers() {
+    //    return users;
+    //}
+//
+//    public void setUsers(Set<User> users) {
+//        this.users = users;
+//    }
 }

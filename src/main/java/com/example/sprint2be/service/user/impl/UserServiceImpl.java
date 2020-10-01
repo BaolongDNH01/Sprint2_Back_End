7cbbc5bfb2b06f8c536d65aed8e69b6a4935cff4
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-10-01 19:12:49 +0700

    quan

--- src/main/java/com/example/sprint2be/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/example/sprint2be/service/user/impl/UserServiceImpl.java
@@ -2,16 +2,17 @@ package com.example.sprint2be.service.user.impl;
 
 import com.example.sprint2be.model.Role;
 import com.example.sprint2be.model.user.User;
+import com.example.sprint2be.model.user.UserDto;
 import com.example.sprint2be.repository.RoleRepository;
 import com.example.sprint2be.repository.UserRepository;
+import com.example.sprint2be.service.rank.RankService;
 import com.example.sprint2be.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -19,68 +20,89 @@ public class UserServiceImpl implements UserService {
     UserRepository userRepository;
     @Autowired
     RoleRepository roleRepository;
-
-    @Override
-    public Boolean save(User user) {
-        if (Boolean.TRUE.equals(findByUsername(user.getUsername()))) {
-            return false;
-        }
+    @Autowired
+    RankService rankService;
+    private User convertToUser(UserDto userDto){
+        User user = new User();
+        user.setUserId(userDto.getUserId());
+        user.setFullName(userDto.getFullName());
+        user.setUsername(userDto.getUsername());
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        user.setPassword(encoder.encode(user.getPassword()));
+        user.setPassword(encoder.encode(userDto.getPassword()));
+        user.setEmail(userDto.getEmail());
+        user.setPhone(userDto.getPhone());
+        user.setBirthday(userDto.getBirthday());
+        user.setAddress(userDto.getAddress());
+        user.setIdCard(userDto.getIdCard());
+        user.setPoint(userDto.getPoint());
+        user.setSignInRecent(userDto.getSignInRecent());
+        user.setFlag(userDto.getFlag());
+        user.setAvatar("https://firebasestorage.googleapis.com/v0/b/real-estate-d8b23.appspot.com/o/mWBlKu8IIggRNhyUutW8?alt=media&token=7f0c3569-e638-4160-bdb5-28cf4dfe22eb");
         Set<Role> roles = new HashSet<>();
         roles.add(roleRepository.findById(2).orElse(null));
         user.setRoles(roles);
-        user.setAvatar("https://firebasestorage.googleapis.com/v0/b/real-estate-d8b23.appspot.com/o/mWBlKu8IIggRNhyUutW8?alt=media&token=7f0c3569-e638-4160-bdb5-28cf4dfe22eb");
-        userRepository.save(user);
-        return true;
+        user.setRank(rankService.findById(userDto.getRankId()));
+        return user;
     }
-
-    @Override
-    public void edit(User user) {
-        userRepository.save(user);
-    }
-
-    @Override
-    public Boolean findByUsername(String username) {
-        return (userRepository.findByUsername(username).orElse(null) != null);
-    }
-
-    @Override
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
-
-    @Override
-    public Boolean deleteUser(Integer id) {
-        if (Boolean.FALSE.equals(userRepository.existsById(id))) {
-            return false;
-        }
-        userRepository.deleteById(id);
-        return true;
+    private UserDto convertToUserDto(User user){
+        UserDto userDto = new UserDto();
+        userDto.setUserId(user.getUserId());
+        userDto.setFullName(user.getFullName());
+        userDto.setUsername(user.getUsername());
+        userDto.setPassword(user.getPassword());
+        userDto.setEmail(user.getEmail());
+        userDto.setPhone(user.getPhone());
+        userDto.setBirthday(user.getBirthday());
+        userDto.setAddress(user.getAddress());
+        userDto.setIdCard(user.getIdCard());
+        userDto.setPoint(user.getPoint());
+        userDto.setSignInRecent(user.getSignInRecent());
+        userDto.setFlag(user.getFlag());
+        userDto.setAvatar(user.getAvatar());
+        userDto.setRankId(user.getRank().getRankId());
+        return userDto;
     }
 
 //    @Override
-//    public User parseDto(UserDto userDto) {
-//        User user = new User();
-//        user.setId(userDto.getId());
-//        user.setUsername(userDto.getUsername());
-//        user.setUserPassword(userDto.getUserPassword());
-//        user.setFullName(userDto.getFullName());
-//        user.setAddress(userDto.getAddress());
-//        user.setEmail(userDto.getEmail()); // THIEN FIXED
-//        user.setPhoneNumber(userDto.getPhoneNumber());
-//        user.setAvatar(userDto.getAvatar());
-//        user.setExamList(new ArrayList<>());
-//        return user ;
+//    public Boolean findByUsername(String username) {
+//        return (userRepository.findByUsername(username).orElse(null) != null);
 //    }
-
-    public User findById(Integer id) {
-        return userRepository.findById(id).orElse(null);
+    @Override
+    public List<UserDto> findAll() {
+        return userRepository.findAll().stream().map(this::convertToUserDto).collect(Collectors.toList());
     }
 
     @Override
-    public User findUserName(String username) {
-        return userRepository.findByUsername(username).orElse(null);
+    public UserDto findById(Integer id) {
+        return (userRepository.findById(id)).map(this::convertToUserDto).orElse(null);
     }
+
+    @Override
+    public void create(UserDto userDto) {
+
+        userRepository.save(convertToUser(userDto));
+    }
+
+    @Override
+    public void delete(Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public void lockUser(UserDto userDto) {
+        userDto.setFlag("false");
+        userRepository.save(convertToUser(userDto));
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                userDto.setFlag("true");
+                userRepository.save(convertToUser(userDto));
+            }
+        };
+//        1000 time = 1s
+        Timer timer = new Timer();
+        timer.schedule(timerTask, userDto.getTimeLock());
+    }
+
 }
 

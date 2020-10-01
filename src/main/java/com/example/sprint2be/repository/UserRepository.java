a26dd5b2668ea4e66d545d4a0b9e150e7fd22d8a
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-10-02 00:39:10 +0700

    quan

--- src/main/java/com/example/sprint2be/repository/UserRepository.java
+++ src/main/java/com/example/sprint2be/repository/UserRepository.java
@@ -13,6 +13,4 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Integer> {
     User findAllByUsername(String username);
     Optional<User> findByUsername(String username);
-
-
 }

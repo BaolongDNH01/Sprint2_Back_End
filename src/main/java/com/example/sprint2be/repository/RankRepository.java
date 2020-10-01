ec8b1da996187bb259b61d572a97a3dc7d4ef0ed
BaoLongDNH01 <duongtranbaolongdn@gmail.com>
2020-10-01 10:34:25 +0700

    deleted

--- src/main/java/com/example/sprint2be/repository/RankRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sprint2be.repository;
-
-import com.example.sprint2be.model.Rank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RankRepository extends JpaRepository<Rank, Integer> {
-}
--- src/main/java/com/example/sprint2be/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sprint2be.repository;
-
-import com.example.sprint2be.model.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}

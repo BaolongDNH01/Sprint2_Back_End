be56e1e77b37757ce0dd8acac929c963133c3e42
BaoLongDNH01 <duongtranbaolongdn@gmail.com>
2020-10-01 10:54:58 +0700

    new project

---
+++ src/test/java/com/example/sprint2be/Sprint2beApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprint2be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint2beApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

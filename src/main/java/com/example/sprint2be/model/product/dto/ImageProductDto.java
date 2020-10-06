c3a806b414e9df9c7fc8d6ddcd6fd3dd69c7f039
chaudang <chaudang1595@gmail.com>
2020-10-06 11:53:27 +0700

    chau update imageProductDto

--- src/main/java/com/example/sprint2be/model/product/dto/ImageProductDto.java
+++ src/main/java/com/example/sprint2be/model/product/dto/ImageProductDto.java
@@ -1,4 +1,35 @@
 package com.example.sprint2be.model.product.dto;
 
 public class ImageProductDto {
+    private Integer imageId;
+    private String imageURL;
+    private Integer productId;
+
+    public ImageProductDto() {
+
+    }
+
+    public Integer getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(Integer imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
 }

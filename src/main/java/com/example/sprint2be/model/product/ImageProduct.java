b22088192eb8de0f7707be23119ba78e74517396
phuong <daut.phuong@gmail.com>
2020-10-12 12:06:45 +0700

    phuong update

--- src/main/java/com/example/sprint2be/model/product/ImageProduct.java
+++ src/main/java/com/example/sprint2be/model/product/ImageProduct.java
@@ -7,6 +7,7 @@ import java.util.List;
 @Table(name = "image")
 public class ImageProduct {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer imageId;
     private String imageURL;
 
